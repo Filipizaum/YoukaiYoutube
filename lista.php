459cbd380464906104a4853e318fab70db91154f
Felipe Lima <filipizaum@gmail.com>
2016-02-21 16:12:06 -0400

    Baixando lista customizada de vídeos
    Signed-off-by: Felipe Lima <filipizaum@gmail.com>

--- lista.php
+++ lista.php
@@ -10,7 +10,7 @@ include 'listaProntos.php';
 $xml = new DOMDocument();
 
 // Load the url's contents into the DOM
-@$xml->loadHTMLFile('https://www.youtube.com/watch?v=Ate-X-RxkxU&list=PLF7BA6CCFB215C13B');
+@$xml->loadHTMLFile('https://www.youtube.com/watch?v=Okz92FBC1NQ&index=1&list=PLsrQAKqWfP3BApU52zO5zfVgezuBWNDzY');
 
 // Empty array to hold all links to return
 $urls = array();
@@ -22,16 +22,17 @@ $items = $list->childNodes;
 /* Pega os que já foram baixados, salvos em um arquivo de texto */
 $listaProntos = pegaListaProntos();
 
+/*
 for ($i = 0; $i < $items->length; ++$i) {
     $item = $items->item($i);
-    /* Se é um elemento e tem o data video id */
+    // Se é um elemento e tem o data video id 
     if(($item instanceof \DOMElement) && ($item->hasAttribute ('data-video-id'))){
         $vid = $item->getAttribute('data-video-id');
-        /* Correção do episódio 15 temporada 2 */
+        // Correção do episódio 15 temporada 2 
         if($vid == 'uKa3BUG2mk0'){
             $vid = 'wfyI0M_cdlA';
         }
-        /* Se o vídeo não foi baixado */
+        // Se o vídeo não foi baixado 
         if (!in_array(toYoutubeUrl($vid).PHP_EOL, $listaProntos)) {
             $arr = array(
                     0 => toYoutubeUrl($vid), 
@@ -39,6 +40,29 @@ for ($i = 0; $i < $items->length; ++$i) {
                     );
             $urls[] = $arr;
         }
+    }
+    
+}
+*/
+
+$vidArr = array(
+	'EiD0ZIr8di8',
+	'_Z_oH_orS80',
+	'MN1mVQoxtYc',
+	'JKWCzKiy3RI',
+	'CQBANIuybDA',
+	'QStDdo5J0lw'
+);
+
+for ($i = 0; $i < count($vidArr); ++$i) {
+    $vid = $vidArr[$i];
+    /* Se o vídeo não foi baixado */
+    if (!in_array(toYoutubeUrl($vid).PHP_EOL, $listaProntos)) {
+        $arr = array(
+                0 => toYoutubeUrl($vid), 
+                1 => 'NY'
+                );
+        $urls[] = $arr;
     }
     
 }
